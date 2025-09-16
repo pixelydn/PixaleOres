2f14c8fbe3c732a84f16b94acff5253360e99e6a
pixely114 <pixelydn@gmail.com>
2025-09-16 19:36:52 -0300

    chromium armor effects

--- src/main/java/pixale/ores/PixalesOres.java
+++ src/main/java/pixale/ores/PixalesOres.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pixale.ores.ores.Chromium;
+import pixale.ores.ores.armor.effects.ChromiumArmorEffect;
 
 public class PixalesOres implements ModInitializer {
 	public static final String MOD_ID = "pixaleores";
@@ -17,6 +18,7 @@ public class PixalesOres implements ModInitializer {
 		LOGGER.info("Yo wassup");
 
 		Chromium.registerChromium();
+		ChromiumArmorEffect.register();
 
 	}
 }
--- src/main/java/pixale/ores/ores/armor/effects/ChromiumArmorEffect.java
+++ src/main/java/pixale/ores/ores/armor/effects/ChromiumArmorEffect.java
@@ -1,90 +1,36 @@
 package pixale.ores.ores.armor.effects;
 
-import com.google.common.collect.ImmutableMap;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.effect.StatusEffectInstance;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.world.World;
-import pixale.ores.util.PixaleArmorMaterials;
+import net.minecraft.server.network.ServerPlayerEntity;
+import pixale.ores.ores.Chromium;
 
-import java.util.List;
-import java.util.Map;
+public class ChromiumArmorEffect {
 
 
-public class ChromiumArmorEffect extends ArmorItem {
-    private static final Map<RegistryEntry<ArmorMaterial>, List<StatusEffectInstance>> MATERIAL_TO_EFFECT_MAP =
-            (new ImmutableMap.Builder<RegistryEntry<ArmorMaterial>, List<StatusEffectInstance>>())
-                    .put(PixaleArmorMaterials.CHROMIUM,
-                            List.of(new StatusEffectInstance(StatusEffects., 400, 2, false, false),
-                                    new StatusEffectInstance(StatusEffects.JUMP_BOOST, 400, 1, false, false))).build();
-
-    public ChromiumArmorEffect(RegistryEntry<ArmorMaterial> material, Type type, Settings settings) {
-        super(material, type, settings);
+    public static boolean hasFullSet(PlayerEntity player) {
+        return player.getInventory().armor.get(3).isOf(Chromium.CHROMIUM_HELMET) &&
+                player.getInventory().armor.get(2).isOf(Chromium.CHROMIUM_CHESTPLATE) &&
+                player.getInventory().armor.get(1).isOf(Chromium.CHROMIUM_LEGGINGS) &&
+                player.getInventory().armor.get(0).isOf(Chromium.CHROMIUM_BOOTS);
     }
 
-    @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if(!world.isClient()) {
-            if(entity instanceof PlayerEntity player) {
-                if(hasFullSuitOfArmorOn(player)) {
-                    evaluateArmorEffects(player);
-                }
-            }
-        }
 
-        super.inventoryTick(stack, world, entity, slot, selected);
-    }
-
-    private void evaluateArmorEffects(PlayerEntity player) {
-        for (Map.Entry<RegistryEntry<ArmorMaterial>, List<StatusEffectInstance>> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
-            RegistryEntry<ArmorMaterial> mapArmorMaterial = entry.getKey();
-            List<StatusEffectInstance> mapStatusEffects = entry.getValue();
-
-            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
-                addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffects);
-            }
+    public static void onArmorTick(PlayerEntity player) {
+        if (hasFullSet(player)) {
+            player.removeStatusEffect(StatusEffects.POISON);
+            player.removeStatusEffect(StatusEffects.WITHER);
         }
     }
 
-    private void addStatusEffectForMaterial(PlayerEntity player, RegistryEntry<ArmorMaterial> mapArmorMaterial, List<StatusEffectInstance> mapStatusEffect) {
-        boolean hasPlayerEffect = mapStatusEffect.stream().allMatch(statusEffectInstance -> player.hasStatusEffect(statusEffectInstance.getEffectType()));
 
-        if(!hasPlayerEffect) {
-            for (StatusEffectInstance instance : mapStatusEffect) {
-                player.addStatusEffect(new StatusEffectInstance(instance.getEffectType(),
-                        instance.getDuration(), instance.getAmplifier(), instance.isAmbient(), instance.shouldShowParticles()));
+    public static void register() {
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+                onArmorTick(player);
             }
-        }
-    }
-
-    private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
-        ItemStack boots = player.getInventory().getArmorStack(0);
-        ItemStack leggings = player.getInventory().getArmorStack(1);
-        ItemStack breastplate = player.getInventory().getArmorStack(2);
-        ItemStack helmet = player.getInventory().getArmorStack(3);
-
-        return !helmet.isEmpty() && !breastplate.isEmpty()
-                && !leggings.isEmpty() && !boots.isEmpty();
-    }
-
-    private boolean hasCorrectArmorOn(RegistryEntry<ArmorMaterial> material, PlayerEntity player) {
-        for (ItemStack armorStack: player.getInventory().armor) {
-            if(!(armorStack.getItem() instanceof ArmorItem)) {
-                return false;
-            }
-        }
-
-        ArmorItem boots = ((ArmorItem)player.getInventory().getArmorStack(0).getItem());
-        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmorStack(1).getItem());
-        ArmorItem breastplate = ((ArmorItem)player.getInventory().getArmorStack(2).getItem());
-        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmorStack(3).getItem());
-
-        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
-                leggings.getMaterial() == material && boots.getMaterial() == material;
+        });
     }
 }
